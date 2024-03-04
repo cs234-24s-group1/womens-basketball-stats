c834dfd3a5817348dcc00e838cba1c3badce450c
megancurtis <curtism@moravian.edu>
2024-03-04 12:44:18 -0500

    Roster class updated

--- app/src/main/java/Roster.java
+++ app/src/main/java/Roster.java
@@ -1,3 +1,36 @@
-public class Roster {
+import java.util.ArrayList;
 
-}
+/*
+ * Represents a roster of players.
+ */
+public class Roster {
+    ArrayList<Player> roster;
+
+    public Roster(){
+        roster = new ArrayList<Player>();
+    }
+    
+    /*
+     * adds a player to the roster
+     */
+    public void addPlayer(String name, String position, int number){
+        Player player = new Player(name, position, number);
+        roster.add(player);
+    }
+
+    /*
+     * removes a player from the roster
+     */
+    public void removePlayer(Player player){
+        roster.remove(player);
+    }
+
+    /*
+     * returns the roster
+     */
+    public void getRoster(){
+
+        }
+    }
+
+
